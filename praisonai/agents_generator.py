26ff94fe3573576be1fec916d3251da7ac36854f
MervinPraison <praison.ms@gmail.com>
2024-04-02 13:09:25 +0530

    Adding AutoGen Tools integration

--- praisonai/agents_generator.py
+++ praisonai/agents_generator.py
@@ -63,6 +63,18 @@ class AgentsGenerator:
             # print(self.config_list)
             llm_config = {"config_list": self.config_list}
             
+            # Assuming the user proxy agent is set up as per your requirements
+            user_proxy = autogen.UserProxyAgent(
+                name="User",
+                human_input_mode="NEVER",
+                is_termination_msg=lambda x: (x.get("content") or "").rstrip().rstrip(".").lower().endswith("terminate") or "TERMINATE" in (x.get("content") or ""),
+                code_execution_config={
+                    "work_dir": "coding",
+                    "use_docker": False,
+                },
+                # additional setup for the user proxy agent
+            )
+            
             for role, details in config['roles'].items():
                 agent_name = details['role'].format(topic=topic).replace("{topic}", topic)
                 agent_goal = details['goal'].format(topic=topic)
@@ -72,6 +84,10 @@ class AgentsGenerator:
                     llm_config=llm_config,
                     system_message=details['backstory'].format(topic=topic)+". Must Reply \"TERMINATE\" in the end when everything is done.",
                 )
+                for tool in details.get('tools', []):
+                    if tool in tools_dict:
+                        tool_class = globals()[f'autogen_{type(tools_dict[tool]).__name__}']
+                        tool_class(agents[role], user_proxy)
 
                 # Preparing tasks for initiate_chats
                 for task_name, task_details in details.get('tasks', {}).items():
@@ -86,18 +102,7 @@ class AgentsGenerator:
                     }
                     tasks.append(chat_task)
 
-            # Assuming the user proxy agent is set up as per your requirements
-            user = autogen.UserProxyAgent(
-                name="User",
-                human_input_mode="NEVER",
-                is_termination_msg=lambda x: (x.get("content") or "").rstrip().rstrip(".").lower().endswith("terminate") or "TERMINATE" in (x.get("content") or ""),
-                code_execution_config={
-                    "work_dir": "coding",
-                    "use_docker": False,
-                },
-                # additional setup for the user proxy agent
-            )
-            response = user.initiate_chats(tasks)
+            response = user_proxy.initiate_chats(tasks)
             result = "### Output ###\n"+response[-1].summary if hasattr(response[-1], 'summary') else ""
         else:
             for role, details in config['roles'].items():
@@ -126,3 +131,91 @@ class AgentsGenerator:
             response = crew.kickoff()
             result = f"### Task Output ###\n{response}"
         return result
+    
+
+# # AutoGen Tools example below
+# from typing import Any, Optional
+# import os
+# from autogen import ConversableAgent
+# from autogen_tools import autogen_ScrapeWebsiteTool
+
+# assistant = ConversableAgent(
+#     name="Assistant",
+#     system_message="You are a helpful AI assistant. "
+#     "You can help with website scraping. "
+#     "Return 'TERMINATE' when the task is done.",
+#     llm_config={"config_list": [{"model": "gpt-3.5-turbo", "api_key": os.environ["OPENAI_API_KEY"]}]},
+# )
+
+# user_proxy = ConversableAgent(
+#     name="User",
+#     llm_config=False,
+#     is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
+#     human_input_mode="NEVER",
+# )
+
+# autogen_ScrapeWebsiteTool(assistant, user_proxy)
+
+# chat_result = user_proxy.initiate_chat(assistant, message="Scrape the official Nodejs website.")
+
+# # CrewAI Tools example below 
+# import os
+# from crewai import Agent, Task, Crew
+# # Importing crewAI tools
+# from crewai_tools import (
+#     DirectoryReadTool,
+#     FileReadTool,
+#     SerperDevTool,
+#     WebsiteSearchTool
+# )
+
+# # Set up API keys
+# os.environ["SERPER_API_KEY"] = "Your Key" # serper.dev API key
+# os.environ["OPENAI_API_KEY"] = "Your Key"
+
+# # Instantiate tools
+# docs_tool = DirectoryReadTool(directory='./blog-posts')
+# file_tool = FileReadTool()
+# search_tool = SerperDevTool()
+# web_rag_tool = WebsiteSearchTool()
+
+# # Create agents
+# researcher = Agent(
+#     role='Market Research Analyst',
+#     goal='Provide up-to-date market analysis of the AI industry',
+#     backstory='An expert analyst with a keen eye for market trends.',
+#     tools=[search_tool, web_rag_tool],
+#     verbose=True
+# )
+
+# writer = Agent(
+#     role='Content Writer',
+#     goal='Craft engaging blog posts about the AI industry',
+#     backstory='A skilled writer with a passion for technology.',
+#     tools=[docs_tool, file_tool],
+#     verbose=True
+# )
+
+# # Define tasks
+# research = Task(
+#     description='Research the latest trends in the AI industry and provide a summary.',
+#     expected_output='A summary of the top 3 trending developments in the AI industry with a unique perspective on their significance.',
+#     agent=researcher
+# )
+
+# write = Task(
+#     description='Write an engaging blog post about the AI industry, based on the research analyst’s summary. Draw inspiration from the latest blog posts in the directory.',
+#     expected_output='A 4-paragraph blog post formatted in markdown with engaging, informative, and accessible content, avoiding complex jargon.',
+#     agent=writer,
+#     output_file='blog-posts/new_post.md'  # The final blog post will be saved here
+# )
+
+# # Assemble a crew
+# crew = Crew(
+#     agents=[researcher, writer],
+#     tasks=[research, write],
+#     verbose=2
+# )
+
+# # Execute tasks
+# crew.kickoff()
