93945fd0dbb6965ee7c6ad418c4b818ac68d8b48
MervinPraison <praison.ms@gmail.com>
2024-06-10 15:54:27 +0100

    Adding Other Tools

--- tools.py
+++ tools.py
@@ -1,8 +1,6 @@
 # tools.py
 from duckduckgo_search import DDGS
 from praisonai_tools import BaseTool
-from langchain_community.tools.pubmed.tool import PubmedQueryRun
-from langchain.utilities.tavily_search import TavilySearchAPIWrapper
 
 class InternetSearchTool(BaseTool):
     name: str = "InternetSearchTool"
@@ -11,14 +9,4 @@ class InternetSearchTool(BaseTool):
     def _run(self, query: str):
         ddgs = DDGS()
         results = ddgs.text(keywords=query, region='wt-wt', safesearch='moderate', max_results=5)
-        return results
-    
-
-class TavilyTool(BaseTool):
-    name: str = "TavilyTool"
-    description: str = "Search Tavily for relevant information based on a query."
-
-    def _run(self, query: str):
-        api_wrapper = TavilySearchAPIWrapper()
-        results = api_wrapper.results(query=query, max_results=5)
         return results
