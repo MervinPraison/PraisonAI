e913c316514efd06f20487b641c54fd21471c669
MervinPraison <praison.ms@gmail.com>
2024-06-13 08:07:40 +0100

    adding MkDocs 2

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -5,7 +5,7 @@ set -e  # Exit if any command fails
 pip install pdoc3 mkdocs mkdocs-material 
 
 # Generate HTML documentation with pdoc
-pdoc --html --skip-errors --template-dir docs/pdoc_template -o docs/api praisonai --force
+pdoc --html --skip-errors -o docs/api praisonai --force
 
 # Build the MkDocs site
 mkdocs build
