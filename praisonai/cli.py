b02108f1f28895d9f39955780a3f14bd7f9ae5ba
MervinPraison <praison.ms@gmail.com>
2024-06-16 06:06:10 +0100

    v0.0.24

--- praisonai/cli.py
+++ praisonai/cli.py
@@ -9,15 +9,22 @@ from crewai import Agent, Task, Crew
 load_dotenv()
 import autogen
 import gradio as gr
-import chainlit as cl
-import asyncio
-import uvicorn
-from chainlit.cli import cli as chainlit_cli
-from chainlit.server import app
 import argparse
 from .auto import AutoGenerator
 from .agents_generator import AgentsGenerator
 from .inbuilt_tools import *
+
+try:
+    from chainlit.cli import chainlit_run
+    CHAINLIT_AVAILABLE = True
+except ImportError:
+    CHAINLIT_AVAILABLE = False
+
+try:
+    import gradio as gr
+    GRADIO_AVAILABLE = True
+except ImportError:
+    GRADIO_AVAILABLE = False
 
 class PraisonAI:
     def __init__(self, agent_file="agents.yaml", framework="", auto=False, init=False):
@@ -110,7 +117,10 @@ class PraisonAI:
             elif args.ui == "chainlit":
                 self.create_chainlit_interface()
             else:
-                self.create_chainlit_interface()
+                # Modify below code to allow default ui
+                agents_generator = AgentsGenerator(self.agent_file, self.framework, self.config_list)
+                result = agents_generator.generate_crew_and_kickoff()
+                return result
         else:
             agents_generator = AgentsGenerator(self.agent_file, self.framework, self.config_list)
             result = agents_generator.generate_crew_and_kickoff()
@@ -135,7 +145,7 @@ class PraisonAI:
         """
         parser = argparse.ArgumentParser(prog="praisonai", description="praisonAI command-line interface")
         parser.add_argument("--framework", choices=["crewai", "autogen"], help="Specify the framework")
-        parser.add_argument("--ui", nargs='?', const='chainlit', default="chainlit", help="Specify the UI framework (gradio or chainlit). Default chainlit")
+        parser.add_argument("--ui", choices=["chainlit", "gradio"], help="Specify the UI framework (gradio or chainlit).")
         parser.add_argument("--auto", nargs=argparse.REMAINDER, help="Enable auto mode and pass arguments for it")
         parser.add_argument("--init", nargs=argparse.REMAINDER, help="Enable auto mode and pass arguments for it")
         parser.add_argument("agent_file", nargs="?", help="Specify the agent file")
@@ -165,40 +175,43 @@ class PraisonAI:
         Example:
             >>> praison_ai.create_gradio_interface()
         """
-        def generate_crew_and_kickoff_interface(auto_args, framework):
-            """
-            Generate a crew and kick off tasks based on the provided auto arguments and framework.
+        if GRADIO_AVAILABLE:
+            def generate_crew_and_kickoff_interface(auto_args, framework):
+                """
+                Generate a crew and kick off tasks based on the provided auto arguments and framework.
 
-            Args:
-                auto_args (list): Topic.
-                framework (str): The framework to use for generating agents.
+                Args:
+                    auto_args (list): Topic.
+                    framework (str): The framework to use for generating agents.
 
-            Returns:
-                str: A string representing the result of generating the crew and kicking off tasks.
+                Returns:
+                    str: A string representing the result of generating the crew and kicking off tasks.
 
-            Raises:
-                None: This method does not raise any exceptions.
+                Raises:
+                    None: This method does not raise any exceptions.
 
-            Example:
-                >>> result = generate_crew_and_kickoff_interface("Create a movie about Cat in Mars", "crewai")
-                >>> print(result)
-            """
-            self.framework = framework
-            self.agent_file = "test.yaml"
-            generator = AutoGenerator(topic=auto_args , framework=self.framework)
-            self.agent_file = generator.generate()
-            agents_generator = AgentsGenerator(self.agent_file, self.framework, self.config_list)
-            result = agents_generator.generate_crew_and_kickoff()
-            return result
+                Example:
+                    >>> result = generate_crew_and_kickoff_interface("Create a movie about Cat in Mars", "crewai")
+                    >>> print(result)
+                """
+                self.framework = framework
+                self.agent_file = "test.yaml"
+                generator = AutoGenerator(topic=auto_args , framework=self.framework)
+                self.agent_file = generator.generate()
+                agents_generator = AgentsGenerator(self.agent_file, self.framework, self.config_list)
+                result = agents_generator.generate_crew_and_kickoff()
+                return result
 
-        gr.Interface(
-            fn=generate_crew_and_kickoff_interface,
-            inputs=[gr.Textbox(lines=2, label="Auto Args"), gr.Dropdown(choices=["crewai", "autogen"], label="Framework")],
-            outputs="textbox",
-            title="Praison AI Studio",
-            description="Create Agents and perform tasks",
-            theme="default"
-        ).launch()
+            gr.Interface(
+                fn=generate_crew_and_kickoff_interface,
+                inputs=[gr.Textbox(lines=2, label="Auto Args"), gr.Dropdown(choices=["crewai", "autogen"], label="Framework")],
+                outputs="textbox",
+                title="Praison AI Studio",
+                description="Create Agents and perform tasks",
+                theme="default"
+            ).launch()
+        else:
+            print("ERROR: Gradio is not installed. Please install it with 'pip install \"praisonai[gradio]\"' to use this feature.") 
         
     def create_chainlit_interface(self):
         """
@@ -211,10 +224,11 @@ class PraisonAI:
         Returns:
             None: This function does not return any value. It starts the Chainlit application.
         """
-        from chainlit.cli import chainlit_run  # Import chainlit_run
-        os.environ["CHAINLIT_PORT"] = "8082"  
-        chainlit_run(["praisonai/chainlit_ui.py"])  
-        
+        if CHAINLIT_AVAILABLE:
+            os.environ["CHAINLIT_PORT"] = "8082"  
+            chainlit_run(["praisonai/chainlit_ui.py"])  
+        else:
+            print("ERROR: Chainlit is not installed. Please install it with 'pip install \"praisonai\[ui]\"' to use the UI.")        
 
 if __name__ == "__main__":
     praison_ai = PraisonAI()
