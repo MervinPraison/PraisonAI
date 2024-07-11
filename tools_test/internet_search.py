e996268d1050fbf2c0e0d207fa773a21ed034ba2
MervinPraison <praison.ms@gmail.com>
2024-07-11 21:46:20 +0100

    Deployed 2f4ad10 with MkDocs version: 1.6.0

---
+++ tools_test/internet_search.py
@@ -0,0 +1,11 @@
+from duckduckgo_search import DDGS
+from praisonai_tools import BaseTool
+
+class InternetSearchTool(BaseTool):
+    name: str = "Internet Search Tool"
+    description: str = "Search Internet for relevant information based on a query or latest news"
+
+    def _run(self, query: str):
+        ddgs = DDGS()
+        results = ddgs.text(keywords=query, region='wt-wt', safesearch='moderate', max_results=5)
+        return results
