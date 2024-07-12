5d6bf972b10939501bc37a42f9c3e0c1fd8199b7
MervinPraison <praison.ms@gmail.com>
2024-07-12 09:01:04 +0100

    Adding Funding Options

--- tests/test_agents_playbook.py
+++ tests/test_agents_playbook.py
@@ -5,21 +5,21 @@ from praisonai import PraisonAI
 
 class TestPraisonAIFramework(unittest.TestCase):
     def test_main_with_autogen_framework(self):
-        praison_ai = PraisonAI(agent_file='tests/autogen-agents.yaml')
-        result = praison_ai.main()
+        praisonai = PraisonAI(agent_file='tests/autogen-agents.yaml')
+        result = praisonai.run()
         self.assertIn('### Task Output ###', result)
 
     def test_main_with_custom_framework(self):
-        praison_ai = PraisonAI(agent_file='tests/crewai-agents.yaml')
-        result = praison_ai.main()
+        praisonai = PraisonAI(agent_file='tests/crewai-agents.yaml')
+        result = praisonai.run()
         self.assertIn('### Task Output ###', result)
 
     def test_main_with_internet_search_tool(self):
-        praison_ai = PraisonAI(agent_file='tests/search-tool-agents.yaml')
-        result = praison_ai.main()
+        praisonai = PraisonAI(agent_file='tests/search-tool-agents.yaml')
+        result = praisonai.run()
         self.assertIn('### Task Output ###', result)
 
     def test_main_with_built_in_tool(self):
-        praison_ai = PraisonAI(agent_file='tests/inbuilt-tool-agents.yaml')
-        result = praison_ai.main()
+        praisonai = PraisonAI(agent_file='tests/inbuilt-tool-agents.yaml')
+        result = praisonai.run()
         self.assertIn('### Task Output ###', result)
