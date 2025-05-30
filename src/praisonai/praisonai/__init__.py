766e5789e3d9a54282b703ae9b735245935c3609
Mervin Praison <praison.ms@gmail.com>
2025-05-30 12:25:14 +0100

    Update __init__.py

--- src/praisonai/praisonai/__init__.py
+++ src/praisonai/praisonai/__init__.py
@@ -3,14 +3,5 @@ import os
 os.environ["OTEL_SDK_DISABLED"] = "true"
 os.environ["EC_TELEMETRY"] = "false"
 
-# Directory management is available for explicit initialization
-try:
-    from .inc.directory_manager import initialize_directories
-    # Note: Directory initialization should be called explicitly via CLI or setup
-    # to avoid unexpected file operations on package import
-except ImportError:
-    # Directory manager is optional for backwards compatibility
-    pass
-
 from .cli import PraisonAI
 from .version import __version__
